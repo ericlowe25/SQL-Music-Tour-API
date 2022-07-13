126fbf2a7527649e9cfba28e8d709c210cc8ed09
Eric Lowe <ericlowe25@gmail.ocm>
2022-07-13 19:36:41 -0400

    added seq. cli

--- models/band.js
+++ models/band.js
@@ -1,6 +1,6 @@
 //DEPENDENCIES
 const{Sequelize, DataTypes, Models, Model} = require('sequelize')
-const sequelize = new Sequelize(process.env.PG_URI)
+// const sequelize = new Sequelize(process.env.PG_URI)
 
 //MODEL
 class Band extends Model{}
